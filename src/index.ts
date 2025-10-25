8cd9cba09185835751cdf71d073a5b97e1c874bf
Pongaluke <51413507+lukeponga-dev@users.noreply.github.com>
2025-10-25 15:36:34 +1300

    Fix error message quotes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { ThemeProvider } from './contexts/ThemeContext';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
